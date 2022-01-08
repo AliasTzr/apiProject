84aa96a5f898ef7ab08a2524b78c820e928b524e
AliasTzr <94296101+AliasTzr@users.noreply.github.com>
2022-01-08 20:48:13 +0100

    Update fonctions.php

--- api/fonctions.php
+++ api/fonctions.php
@@ -1,7 +1,7 @@
 <?php
  function getProducts(){
     $connect = connexion();
-    $statut = pg_query($connect, "SELECT * FROM produits");
+    $statut = pg_query($connect, "SELECT * FROM products");
     $produits = pg_fetch_all($statut);
     $infos = sendToUser($produits);
     echo "{products: $infos}";
@@ -9,7 +9,7 @@
 
  function getProductById($id){
     $connect = connexion();
-    $statut = pg_query($connect, "SELECT * FROM produits WHERE id = $id");
+    $statut = pg_query($connect, "SELECT * FROM products WHERE id = $id");
     $produits = pg_fetch_all($statut);
     return sendToUser($produits);
     
@@ -20,11 +20,11 @@ function insertDataFromUser($name, $description, $price, $inStock)
     $connect = connexion();
     if($inStock == "f")
     {
-        pg_insert($connect, 'produits', array('name'=>$name, 'description'=>$description, 'price'=>$price, 'inStock'=>FALSE));
+        pg_insert($connect, 'products', array('name'=>$name, 'description'=>$description, 'price'=>$price, 'inStock'=>FALSE));
     }
     if($inStock == "t")
     {
-        pg_insert($connect, 'produits', array('name'=>$name, 'description'=>$description, 'price'=>$price, 'inStock'=>TRUE));
+        pg_insert($connect, 'products', array('name'=>$name, 'description'=>$description, 'price'=>$price, 'inStock'=>TRUE));
     }
 }
 
@@ -32,21 +32,21 @@ function updateName($name, $id)
 {
     $connect = connexion();
     //$stat = pg_update($connect, 'produits', array('id'=>$id), array('name'=>$name));
-    pg_query($connect, "UPDATE produits SET name = '$name' WHERE id = $id");
+    pg_query($connect, "UPDATE products SET name = '$name' WHERE id = $id");
     return "{ message: 'Modified!' }";
 }
 
 function updateDescription($description, $id)
 {
     $connect = connexion();
-    pg_query($connect, "UPDATE produits SET description = '$description' WHERE id = $id");
+    pg_query($connect, "UPDATE products SET description = '$description' WHERE id = $id");
     return "{ message: 'Modified!' }";
 }
 
 function updatePrice($price, $id)
 {
     $connect = connexion();
-    pg_query($connect, "UPDATE produits SET price = '$price' WHERE id = $id");
+    pg_query($connect, "UPDATE products SET price = '$price' WHERE id = $id");
     return "{ message: 'Modified!' }";
 }
 
@@ -55,11 +55,11 @@ function updateInStock($inStock, $id)
     $connect = connexion();
     if($inStock[0] == "f")
     {
-        pg_query($connect, "UPDATE produits SET inStock = FALSE WHERE id = $id");
+        pg_query($connect, "UPDATE products SET inStock = FALSE WHERE id = $id");
     }
     if($inStock[0] == "t")
     {
-        pg_query($connect, "UPDATE produits SET inStock = TRUE WHERE id = $id");
+        pg_query($connect, "UPDATE products SET inStock = TRUE WHERE id = $id");
     }
     return "{ message: 'Modified!' }";
 }
@@ -67,12 +67,12 @@ function updateInStock($inStock, $id)
 function deleteDataFromUser($id)
 {
     $connect = connexion();
-    pg_query($connect, "DELETE FROM produits WHERE id = $id");
+    pg_query($connect, "DELETE FROM products WHERE id = $id");
     return "{ message: 'Deleted!' }";
 }
 
 function connexion(){
-    return pg_connect("host=localhost dbname=Product user=postgres password=root");
+    return pg_connect("host=ec2-34-206-245-175.compute-1.amazonaws.com dbname=dfhg0uoil21hl7 user=jzvzsfcurpgzvv password=    fa4871d3ecd117e4da21a28807c9b9fe20f26ec9adb0788ef03169a2b026399b");
 }
 
 function sendToUser($infos){
