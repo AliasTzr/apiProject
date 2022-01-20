00c8ce7ce18c6a09103aa6a763773a3ebfb4dd2c
AliasTzr <94296101+AliasTzr@users.noreply.github.com>
2022-01-20 12:50:56 +0000

    Update fonctions.php

--- api/fonctions.php
+++ api/fonctions.php
@@ -19,7 +19,7 @@ function insertDataFromUser($name, $description, $price, $inStock)
 {
     $connect = connexion();
     $result = pg_prepare($connect, "my_query", 'INSERT INTO products (name, description, price, inStock) VALUES ($1, $2, $3, $4)');
-    $result = pg_execute($connect, "my_query", array($name, $description, $price, ($inStock == 't')));
+    $result = pg_execute($connect, "my_query", array($name, $description, $price, $inStock === 't' ? TRUE : FALSE));
 //     if($inStock == "f")
 //     {
 //         pg_query($connect, "INSERT INTO products (name, description, price, inStock) VALUES ('$name', $price, '$description', FALSE)");
